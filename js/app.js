bebfcd345e6c471a10c78f0d9211d48d83eddf02
Bert Nienhuis <Bert Nienhuis>
2016-12-30 23:54:29 +0100

    First push

---
+++ js/app.js
@@ -0,0 +1,16 @@
+var app = angular.module("myApp", ["ngRoute"]);
+app.config(function($routeProvider) {
+    $routeProvider
+    .when("/", {
+        templateUrl : "templates/korea_frequences.html"
+    })
+    .when("/korea_frequences", {
+        templateUrl : "templates/korea_frequences.html"
+    })
+    .when("/korea_map", {
+        templateUrl : "templates/korea_map.html"
+    })
+    .when("/korea_airports", {
+        templateUrl : "templates/korea_airports.html"
+    });
+});
