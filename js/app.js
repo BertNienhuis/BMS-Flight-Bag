234f2c57016322678c5e7bcf3ab8f0298eeffcc5
B. Nienhuis <b.nienhuis@hotmail.com>
2016-12-31 09:49:05 +0100

    Fixed Korea links

--- js/app.js
+++ js/app.js
@@ -5,8 +5,8 @@ app.config(function($routeProvider) {
     .when("/", {
         templateUrl : "templates/korea_frequences.html"
     })
-    .when("/korea_frequences", {
-        templateUrl : "templates/korea_frequences.html"
+    .when("/korea_frequencies", {
+        templateUrl : "templates/korea_frequencies.html"
     })
     .when("/korea_map", {
         templateUrl : "templates/korea_map.html"
