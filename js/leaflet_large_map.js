d75dfbb01b912d540016a63b99d05542d39086a8
B. Nienhuis <b.nienhuis@hotmail.com>
2016-12-31 12:24:23 +0100

    no message

--- js/leaflet_large_map.js
+++ js/leaflet_large_map.js
@@ -1,28 +1,28 @@
- Using leaflet.js to pan and zoom a big image.
- See also httpkempe.netblog20140614leaflet-pan-zoom-image.html
+// Using leaflet.js to pan and zoom a big image.
+// See also: http://kempe.net/blog/2014/06/14/leaflet-pan-zoom-image.html
 
- create the slippy map
+// create the slippy map
 var map = L.map('image-map', {
-  minZoom 1,
-  maxZoom 4,
-  center [0, 0],
-  zoom 1,
-  crs L.CRS.Simple
+  minZoom: 1,
+  maxZoom: 4,
+  center: [0, 0],
+  zoom: 1,
+  crs: L.CRS.Simple
 });
 
- dimensions of the image
+// dimensions of the image
 var w = 2000,
     h = 1500,
-    url = 'https://seoscale.nl/wp-content/uploads/2016/04/https-verbinding.jpg';
+    url = 'http://kempe.net/images/newspaper-big.jpg';
 
- calculate the edges of the image, in coordinate space
+// calculate the edges of the image, in coordinate space
 var southWest = map.unproject([0, h], map.getMaxZoom()-1);
 var northEast = map.unproject([w, 0], map.getMaxZoom()-1);
 var bounds = new L.LatLngBounds(southWest, northEast);
 
- add the image overlay, 
- so that it covers the entire map
+// add the image overlay, 
+// so that it covers the entire map
 L.imageOverlay(url, bounds).addTo(map);
 
- tell leaflet that the map is exactly as big as the image
+// tell leaflet that the map is exactly as big as the image
 map.setMaxBounds(bounds);
