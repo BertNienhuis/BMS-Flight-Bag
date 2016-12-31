5f264a314e8a0fb565bcb5551935dbe667ce742a
B. Nienhuis <b.nienhuis@hotmail.com>
2016-12-31 09:59:00 +0100

    no message

--- js/app.js
+++ js/app.js
@@ -2,7 +2,7 @@
 var app = angular.module("myApp", ["ngRoute"]);
 app.config(function($routeProvider) {
     $routeProvider
-    .when("#/", {
+    .when("/", {
         templateUrl : "templates/korea_frequencies.html"
     })
     .when("/korea_frequencies", {
